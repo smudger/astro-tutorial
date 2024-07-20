fe97415999f2ecd2e788209ccb3119a74e9e9577
Louis Smith <15144861+smudger@users.noreply.github.com>
2024-07-20 17:24:09 +0100

    Extract posts to content collection

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,11 +1,18 @@
-import rss, { pagesGlobToRssItems } from '@astrojs/rss';
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
 
 export async function GET(context) {
+    const posts = await getCollection('posts');
     return rss({
         title: 'Astro Learner | Blog',
         description: 'My journey learning Astro',
         site: context.site,
-        items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
         customData: `<language>en-us</language>`,
+        items: posts.map((post) => ({
+            title: post.data.title,
+            description: post.data.description,
+            pubDate: post.data.pubDate,
+            link: `/posts/${post.slug}/`,
+        })),
     });
 }
