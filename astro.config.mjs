5641a904c00d104136d8748ee4f6c3f23667f4d2
Louis Smith <15144861+smudger@users.noreply.github.com>
2024-07-20 17:08:00 +0100

    Complete tutorial using astro islands

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://profound-dasik-da3fa6.netlify.app/"
+  site: "https://profound-dasik-da3fa6.netlify.app/",
+  integrations: [preact()]
 });
