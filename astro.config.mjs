ecf228da346c23d36314d09df138dcf24482480a
Louis Smith <15144861+smudger@users.noreply.github.com>
2024-07-20 13:37:32 +0100

    Add RSS feed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://profound-dasik-da3fa6.netlify.app/"
+});
