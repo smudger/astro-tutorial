b9c0c8cad46364661f4ef2f6bc3ec7b4990e5bba
Louis Smith <15144861+smudger@users.noreply.github.com>
2024-07-21 12:14:16 +0100

    Add storyblok integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,22 @@
-import { defineConfig } from 'astro/config';
-
 import preact from "@astrojs/preact";
+import storyblok from '@storyblok/astro';
+import { defineConfig } from 'astro/config';
+import { loadEnv } from 'vite';
+
+const env = loadEnv("", process.cwd(), 'STORYBLOK');
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://profound-dasik-da3fa6.netlify.app/",
-  integrations: [preact()]
+  integrations: [
+    preact(),
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      components: {
+        blogPost: 'storyblok/BlogPost',
+        blogPostList: 'storyblok/BlogPostList',
+        page: 'storyblok/Page',
+      },
+    })
+  ]
 });
